23e688c618412ffe6b8171d18f1f094236f73e44
tegu1365 <tedi.d.petkova@gmail.com>
2021-01-10 19:40:52 +0200

    Solved hw4 problem 1

--- UP_20-21_fn62538_d4/fn62538_d4_1_gcc.cpp
+++ UP_20-21_fn62538_d4/fn62538_d4_1_gcc.cpp
@@ -14,6 +14,48 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-return 0;
+
+bool increasingWithMistakes(int *arr, int n, int k)
+{
+
+    for (int i = 0; i < n-1; i++)
+    {
+        if (arr[i] > arr[i+1])
+        {
+            //cout<<arr[i]<<">"<<arr[i+1]<<endl;
+            k--;
+        }
+    }
+
+    if(k<0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    int k=0, n=0, *arr;
+    cin>>k;
+    if(k<0){
+        cout<<-1;
+        return 0;
+    }
+    cin>>n;
+    if(n<0){
+        cout<<-1;
+        return 0;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin>>*(arr+i);
+    }
+
+    cout<<increasingWithMistakes(arr,n,k);
+
+    return 0;
 }
